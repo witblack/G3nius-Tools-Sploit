53c095683ae4d224f60c24af7751277c949d0f2c
root <root@WitBlack>
2021-04-19 09:49:42 +0430

    Version 1.0.5 of G3nius-tools for Linux users published.

--- Linux/lib/functionsPyTwo.py
+++ Linux/lib/functionsPyTwo.py
@@ -6,7 +6,7 @@ import time
 from termcolor import colored
 OS = None
 Location = os.path.dirname(os.path.abspath(__file__))
-Version = '1.0.4'
+Version = '1.0.5'
 Type = 'Linux' # can be : ARM - Windows - Linux
 if str.lower(os.name) == 'nt':
 	OS = 'Windows'
