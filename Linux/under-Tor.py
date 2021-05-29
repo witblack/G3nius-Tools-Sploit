d8b7e966ad1a0acd13557ac0ccba0634b5927d17
WitBlack <admin@bugzone.ir>
2021-05-29 17:47:56 +0430

    run under tor file for linux users on version 1.0.7 changed

--- Linux/under-Tor.py
+++ Linux/under-Tor.py
@@ -3,12 +3,11 @@
 try:
 	from sys import exit
 	import os
-	from termcolor import colored
 except:
-	print("Some Deepends not installed. run 'pip install -r requires.txt'")
+	print("\x1b[0;31mSome Deepends not installed. run 'pip install -r requires.txt'\x1b[0m")
 else:
 	if '' in os.popen('proxychains').read():
 		os.system('proxychains ./launcher.py')
 	else:
-		print(colored("[!] proxychains script not installed. install and retry.",'red'))
+		print("\x1b[0;31m[!] proxychains script not installed. install and retry.\x1b[0m")
 		exit(1)
