0f23251a5861aa1fed0a0a129e28f0bebac3665e
WitBlack <witblack.80@gmail.com>
2022-07-31 16:54:32 +0430

    New config files added

--- G3nius-Tools/lib/config/GPL.py
+++ G3nius-Tools/lib/config/GPL.py
@@ -1,5 +1,11 @@
 # network attackers
-Default_TTL=128
+Default_TTL = 128
 ICMP_Timeout = 2 # sec
 ARP_Timeout = 2 # sec
 ARP_Spoof_Resend_Sleep = 1 # sec
+Check_WiFi_Channel_Changed_In_Change = 0.25 # sec
+Change_Channel_Try_Times = 20
+Default_WiFi_ScanTime = 10 # sec
+AirCrack_More_Arguments_Scan = '--write-interval 1' # If Change that, Are not should change output
+AirCrack_More_Arguments_Capture = ''
+AirCrack_More_Arguments_Fake_Packet = '--ignore-negative-one'
