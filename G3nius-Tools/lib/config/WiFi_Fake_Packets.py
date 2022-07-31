10f7500b3d91d6ca58d12d957bd11c18296bd276
WitBlack <witblack.80@gmail.com>
2022-07-31 17:22:50 +0430

    GPL wifi attackers config files added

---
+++ G3nius-Tools/lib/config/WiFi_Fake_Packets.py
@@ -0,0 +1,12 @@
+# Packets types
+class Packets:
+    Deauth = 0
+    Fake_Auth = 1
+    Interactive = 2
+    ArpReplay = 3
+    ChopChop = 4
+    Fragment = 5
+    Caffe_Latte = 6
+    CFrag = 7
+    MigMode = 8
+    Test = 9
