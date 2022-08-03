c85bc8a8e88d6b94c6d9a33052b1d7f312083a6a
WitBlack <witblack.80@gmail.com>
2022-08-03 11:57:23 +0430

    Version 2.0.7 published

--- G3nius-Tools/lib/config/Main_Configs.py
+++ G3nius-Tools/lib/config/Main_Configs.py
@@ -1,5 +1,5 @@
 """     G3nius core     """
-Version = '2.0.6'
+Version = '2.0.7'
 Type = 'Linux' # can be = ARM - Windows - Linux
 License = 'FREE' # can be = FREE - PREMIUM
 Sleep_On_Unknown_OS = 5
