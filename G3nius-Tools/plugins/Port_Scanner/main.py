5bd98228c17755122be2cd193d7ca08c48a23fe9
WitBlack <witblack.80@gmail.com>
2023-02-15 02:55:05 +0330

    version 2.0.1 of Port_Scanner plugin published, subscribe to new function.

--- G3nius-Tools/plugins/Port_Scanner/main.py
+++ G3nius-Tools/plugins/Port_Scanner/main.py
@@ -5,10 +5,10 @@ from lib.GPL.IO import gpl_input
 from lib.core.Error_Handler import Handler
 import lib.config.Error_Levels as Error_Levels
 from lib.GPL.Page_Managers import gpl_clear_and_banner, gpl_set_banner_verion
-from lib.GPL.attackers.protocols.Service_Version_Name_Scanner import gpl_get_server_service_version
+from lib.GPL.attackers.protocols.Service_Version_Name_Scanner import gpl_find_service_version_name
 
 
-gpl_set_banner_verion('2.0.0')
+gpl_set_banner_verion('2.0.1')
 
 """     get info       """
 IP = gpl_input("Target IP: ", get_ip=True)
@@ -17,7 +17,7 @@ Port = gpl_input("Target port: ", get_port=True)
 """     attack      """
 gpl_clear_and_banner()
 Handler(Error_Levels.Info, "Scanning...")
-Details = gpl_get_server_service_version(IP, Port)
+Details = gpl_find_service_version_name(IP, Port)
 gpl_clear_and_banner()
 if Details == None:
     Handler(Error_Levels.Failed_Job, "Port is off.")
