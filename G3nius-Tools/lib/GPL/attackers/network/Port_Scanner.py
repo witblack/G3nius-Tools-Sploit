07c1de052587b40553ed679e6eef458c493c6ea4
WitBlack <witblack.80@gmail.com>
2023-02-15 02:12:58 +0330

    version 2 of GPL attacker Port_Scanner published

--- G3nius-Tools/lib/GPL/attackers/network/Port_Scanner.py
+++ G3nius-Tools/lib/GPL/attackers/network/Port_Scanner.py
@@ -1,33 +1,52 @@
 """     libs        """
 from nmap import PortScanner
-
-"""     local   """
-Scanner = PortScanner()
+from socket import socket
 
 """     GPL     """
 # scan port status
 #
 # external:
-# from socket import gethostbyname_ex
+# from nmap import PortScanner
 #
 # note:
 # if return None, connection lose or unable to scan
 #
 # version
-# 1
-def gpl_port_scanner(IP, Port=None, UDP=False, more_nmap_argument=''):
-    global Scanner
+# 2
+def gpl_port_scanner(IP, Port=None, UDP=False, Scan_Version=False, More_Nmap_Argument=''):
+    Scanner = PortScanner()
     try:
+        if Scan_Version:
+            More_Nmap_Argument += ' -sV'
         if UDP:
-            more_nmap_argument += ' -sU'
+            More_Nmap_Argument += ' -sU'
         if Port == None:
-            Scan = Scanner.scan(IP, arguments=more_nmap_argument)
+            Scan = Scanner.scan(IP, arguments=More_Nmap_Argument)
         else:
-            Scan = Scanner.scan(IP, str(Port), arguments=more_nmap_argument)
+            Scan = Scanner.scan(IP, str(Port), arguments=More_Nmap_Argument)
         if UDP:
             Result = Scan['scan'][IP]['udp']
         else:
             Result = Scan['scan'][IP]['tcp']
         return Result
     except:
+        return None
+
+
+
+# is tcp open ?
+#
+# external:
+# from socket import socket
+#
+# version
+# 1
+def gpl_is_TCP_open(IP, Port):
+    Socket = socket()
+    Result = Socket.connect_ex((IP, Port))
+    if Result == 0:
+        return True
+    elif Result == 111:
+        return False
+    else:
         return None
