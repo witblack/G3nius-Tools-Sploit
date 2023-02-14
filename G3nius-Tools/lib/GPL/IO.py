968c94f3e0844c66fa064db30a4b7a2a5c3e5bd6
WitBlack <witblack.80@gmail.com>
2023-02-15 02:03:20 +0330

    Version 2 of GPL IO published

--- G3nius-Tools/lib/GPL/IO.py
+++ G3nius-Tools/lib/GPL/IO.py
@@ -1,14 +1,20 @@
 """     libs    """
+# External
 from re import match
 from time import sleep
 from ipaddress import ip_address
 from lib.packages.termcolor import colored
+
+# Internal
 from lib.GPL.HTTP_Managers import gpl_http_get
-import lib.config.Exit_Codes as Exit_Codes
 from lib.GPL.Page_Managers import gpl_clear_and_banner, gpl_clear
-from lib.core.Exit_Request import Exit_Request
 from lib.core.Error_Handler import Handler
+from lib.core.Exit_Request import Exit_Request
+
+# Configs
+import lib.config.Exit_Codes as Exit_Codes
 import lib.config.Error_Levels as Error_Levels
+from lib.config.Main_Configs import Default_Sleep_Time
 
 """     GPL     """
 
@@ -26,8 +32,8 @@ import lib.config.Error_Levels as Error_Levels
 # gpl_get
 #
 # version:
-# 1
-def gpl_input(text: str, dont_style=False, forground_color='white', clear_and_banner_before=True, on_invalid_after_clear_text=None, clear_before=False, on_exit_request_text='\nExit with user request.', on_exit_request_forground_color='yellow', q_to_exit=True, clear_and_banner_when_exit=True, clear_when_exit=False, get_int=False, get_URL=False, get_float=False, get_port=False, get_ip=False, get_MAC=False, on_invalid_number_text='Invalid number!', on_invalid_sleep_by_sec=1, on_invalid_clear_and_banner=True, on_invalid_ip_text="Invalid IP!", on_invalid_mac_text="Invalid MAC!", on_invalid_url_text='Invalid URL or problem to connection !'):
+# 2
+def gpl_input(text: str, dont_style=False, clear_and_banner_before=True, on_invalid_after_clear_text=None, clear_before=False, q_to_exit=True, clear_and_banner_when_exit=True, clear_when_exit=False, get_int=False, get_URL=False, get_float=False, get_port=False, get_ip=False, get_MAC=False, on_invalid_clear_and_banner=True):
     if clear_and_banner_before and (not clear_before):
         gpl_clear_and_banner()
     elif clear_before:
@@ -37,21 +43,20 @@ def gpl_input(text: str, dont_style=False, forground_color='white', clear_and_ba
             if dont_style:
                 choose = input(text)
             else:
-                choose = input(colored(text, forground_color))
+                choose = input(colored(text, 'white'))
             if q_to_exit and (str.lower(choose) == 'q' or str.lower(choose) == 'exit'):
                 if clear_when_exit:
                     gpl_clear()
                 elif clear_and_banner_when_exit:
                     gpl_clear_and_banner()
-                print(colored(on_exit_request_text, on_exit_request_forground_color))
-                Exit_Request(Exit_Codes.Normal)
+                Exit_Request(Exit_Codes.Normal, text_after_clear=colored('Exit with user request.', 'yellow'))
             else:
                 if get_float:
                     try:
                         choose = float(choose)
                     except:
-                        Handler(Error_Levels.High, on_invalid_number_text)
-                        gpl_sleep(on_invalid_sleep_by_sec)
+                        Handler(Error_Levels.High, 'Invalid number!')
+                        gpl_sleep()
                         if on_invalid_clear_and_banner:
                             gpl_clear_and_banner()
                         if on_invalid_after_clear_text:
@@ -62,8 +67,8 @@ def gpl_input(text: str, dont_style=False, forground_color='white', clear_and_ba
                     try:
                         choose = int(choose)
                     except:
-                        Handler(Error_Levels.High, on_invalid_number_text)
-                        gpl_sleep(on_invalid_sleep_by_sec)
+                        Handler(Error_Levels.High, 'Invalid number!')
+                        gpl_sleep()
                         if on_invalid_clear_and_banner:
                             gpl_clear_and_banner()
                         if on_invalid_after_clear_text:
@@ -80,8 +85,8 @@ def gpl_input(text: str, dont_style=False, forground_color='white', clear_and_ba
                     try:
                         ip_address(choose)
                     except:
-                        Handler(Error_Levels.High, on_invalid_ip_text)
-                        gpl_sleep(on_invalid_sleep_by_sec)
+                        Handler(Error_Levels.High, "Invalid IP address!")
+                        gpl_sleep()
                         if on_invalid_clear_and_banner:
                             gpl_clear_and_banner()
                         if on_invalid_after_clear_text:
@@ -92,8 +97,8 @@ def gpl_input(text: str, dont_style=False, forground_color='white', clear_and_ba
                     if match("^(?:[0-9A-Fa-f]{2}[:-]){5}(?:[0-9A-Fa-f]{2})$", choose):
                         return choose
                     else:
-                        Handler(Error_Levels.High, on_invalid_mac_text)
-                        gpl_sleep(on_invalid_sleep_by_sec)
+                        Handler(Error_Levels.High, "Invalid MAC address!")
+                        gpl_sleep()
                         if on_invalid_clear_and_banner:
                             gpl_clear_and_banner()
                         if on_invalid_after_clear_text:
@@ -102,11 +107,10 @@ def gpl_input(text: str, dont_style=False, forground_color='white', clear_and_ba
                     if gpl_http_get(choose) != None:
                         return choose
                     else:
-                        Handler(Error_Levels.Failed_Job, on_invalid_url_text)
+                        Handler(Error_Levels.Failed_Job, 'Invalid URL or problem to connection !')
                 else:
                     return choose
     except (KeyboardInterrupt, EOFError):
-        print(colored("\n" + on_exit_request_text, on_exit_request_forground_color))
         Exit_Request(Exit_Codes.CTRL_C, text_after_clear=colored('Exit with user request.', 'yellow'))
 
 
@@ -129,31 +133,30 @@ def gpl_input(text: str, dont_style=False, forground_color='white', clear_and_ba
 #
 # version:
 # 2
-def gpl_while_input(title: str, list_of_chooses=[], text_color='magenta', number_color='green', title_color='white', input_text='Number of choose : ', input_color='blue', on_invalid_sleep_time=1, on_invalid_text='Invalid number!', on_exit_request_text='Exit with user request.', on_exit_request_forground_text_color='yellow', exit_option=True, exit_option_text='Exit (or "q").'):
+def gpl_while_input(title: str, list_of_chooses: list, exit_option=True):
     if exit_option:
-        list_of_chooses.append(exit_option_text)
+        list_of_chooses.append('Exit (or "q").')
     while True:
         gpl_clear_and_banner()
-        print(colored(title, title_color))
+        print(colored(title, 'white'))
         i = 0
         for option in list_of_chooses:
             i += 1
-            print("\t" + colored(str(i), number_color) + ' ' + colored(option, text_color))
-        choose = gpl_input("\n" + input_text, input_color, clear_and_banner_before=False)
+            print("\t" + colored(str(i), 'green') + ' ' + colored(option, 'magenta'))
+        choose = gpl_input("\nNumber of choose : ", clear_and_banner_before=False)
         try:
             choose = int(choose)
         except:
-            Handler(Error_Levels.High, on_invalid_text)
-            gpl_sleep(on_invalid_sleep_time)
+            Handler(Error_Levels.High, 'Invalid number!')
+            gpl_sleep(1)
             continue
         else:
             if choose <= 0 or choose > len(list_of_chooses):
-                Handler(Error_Levels.High, on_invalid_text)
-                gpl_sleep(on_invalid_sleep_time)
+                Handler(Error_Levels.High, 'Invalid number!')
+                gpl_sleep(1)
                 continue
         if exit_option and choose == len(list_of_chooses):
-            print(colored(on_exit_request_text, on_exit_request_forground_text_color))
-            Exit_Request(Exit_Codes.Normal)
+            Exit_Request(Exit_Codes.Normal, text_after_clear='Exit with user request.')
         else:
             list_of_chooses.pop()
             return choose
@@ -167,16 +170,12 @@ def gpl_while_input(title: str, list_of_chooses=[], text_color='magenta', number
 # from sys import exit
 #
 # version:
-# 1
-def gpl_sleep(time_by_sec=1, on_exit_request_text='Exit with user request.', on_exit_request_forground_color='yellow', manage_ctrl_C_and_exit=True):
+# 2
+def gpl_sleep(time_by_sec=Default_Sleep_Time):
     try:
         sleep(time_by_sec)
     except (KeyboardInterrupt, EOFError):
-        if manage_ctrl_C_and_exit:
-            print(colored(on_exit_request_text, on_exit_request_forground_color))
-            exit(Exit_Codes.CTRL_C)
-        else:
-            raise KeyboardInterrupt
+        Exit_Request(Exit_Codes.Normal, text_after_clear='Exit with user request.')
 
 
 # confirm
