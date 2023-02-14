523a4f835dd927887f25b0357d25d6f5d2eb4f92
WitBlack <witblack.80@gmail.com>
2023-02-15 02:25:26 +0330

    sync with new update some functions

--- G3nius-Tools/lib/core/main/Statistics.py
+++ G3nius-Tools/lib/core/main/Statistics.py
@@ -1,9 +1,10 @@
 """     libs       """
 from lib.GPL.HTTP_Managers import gpl_http_get
+from lib.config.Main_Configs import Statistics_Reports
 
 """     defs       """
-def Send_Statistics(Report, Type="Usage"):
-    if Report:
+def Send_Statistics(Type="Usage"):
+    if Statistics_Reports:
         if Type == 'Usage':
             gpl_http_get("https://api.bugzone.ir/G3nius/Statistics.php?Type=Usage", ok_http_codes=[200])
         else:
