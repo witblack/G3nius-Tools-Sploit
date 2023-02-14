4f4ae871148ac2a92e04738a53c769a6cf45305c
WitBlack <witblack.80@gmail.com>
2023-02-15 02:02:05 +0330

    some config options edited/added

--- G3nius-Tools/lib/config/GPL.py
+++ G3nius-Tools/lib/config/GPL.py
@@ -9,3 +9,6 @@ Default_WiFi_ScanTime = 10 # sec
 AirCrack_More_Arguments_Scan = '--write-interval 1' # If Change that, Are not should change output
 AirCrack_More_Arguments_Capture = ''
 AirCrack_More_Arguments_Fake_Packet = '--ignore-negative-one'
+SSH_Timeout = 10  # sec
+Encoding = 'utf-8'
+Macro_Apply_Time = 0.0001
