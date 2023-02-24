8d1dc3f42649bd7e2b0ec2f7f7d913710cf7a03b
WitBlack <witblack.80@gmail.com>
2023-02-24 20:01:37 +0330

    New version of GoPremium published

--- G3nius-Tools/plugins/GoPremium/main.py
+++ G3nius-Tools/plugins/GoPremium/main.py
@@ -14,10 +14,10 @@ if Price == None:
     Handler(Error_Levels.Critical, "Failed to connect server for get price. Check your internet connection.",)
 else:
     # show
-    Handler(Error_Levels.Alert, "It's only \x1b[6;32m" + Price + "\x1b[0m per month!")
-    Handler(Error_Levels.Alert, "\n\nFor buy it send message with once of following liks:")
-    Handler(Error_Levels.Alert, "\tWeb: https://bugzone.ir/")
+    Handler(Error_Levels.Alert, "It's only \x1b[6;32m" + Price.text + "\x1b[0m per month!")
+    Handler(Error_Levels.Alert, "\n\nFor buy it send message with once of following links:")
+    Handler(Error_Levels.Alert, "\tWebsite: https://bugzone.ir/")
     Handler(Error_Levels.Alert, "\tE-Mail: admin@bugzone.ir")
-    Handler(Error_Levels.Alert, "\tInstagram: https://instagram.com/WitBlack80")
+    Handler(Error_Levels.Alert, "\tInstagram: https://instagram.com/WitBlack_pv")
     Handler(Error_Levels.Alert, "\tTelegram: https://t.me/WitBlack")
     Handler(Error_Levels.Alert, "\tPhone: +98 9379446362\n\n")
