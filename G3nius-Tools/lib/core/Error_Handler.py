e20dedc33cc6ae3f9965360e7c98aaaae0a7bb12
WitBlack <witblack.80@gmail.com>
2023-03-18 20:05:27 +0330

    New version of Error_Handler with End parameter published.

--- G3nius-Tools/lib/core/Error_Handler.py
+++ G3nius-Tools/lib/core/Error_Handler.py
@@ -5,25 +5,30 @@ from lib.packages.termcolor import colored
 from lib.core.Exit_Request import Exit_Request
 
 """     error handler       """
-def Handler(Error_Level, Title, Description=None, Clear_Page=True):
+def Handler(Error_Level, Title, Description=None, Clear_Page=True, Print_Before=None, Without_Newline=False):
+    End = "\n"
+    if Print_Before:
+        print(Print_Before, end='')
+    if Without_Newline:
+        End = ''
     if Error_Level == Error_Levels.Critical:
-        print(colored('[!] ', 'red') + colored(Title, 'yellow'))
+        print(colored('[!] ', 'red') + colored(Title, 'yellow'), end=End)
         if Clear_Page:
             Exit_Request(Exit_Codes.Crash)
         else:
             Exit_Request(Exit_Codes.Crash, clear_and_banner=False)
     elif Error_Level == Error_Levels.High:
-        print(colored('[!] ', 'red') + colored(Title, 'yellow'))
+        print(colored('[!] ', 'red') + colored(Title, 'yellow'), end=End)
     elif Error_Level == Error_Levels.Confirm:
-        print(colored('[?] ', 'yellow') + colored(Title, 'white'))
+        print(colored('[?] ', 'yellow') + colored(Title, 'white'), end=End)
     elif Error_Level == Error_Levels.Failed_Job:
-        print(colored('[-] ', 'yellow') + colored(Title, 'red'))
+        print(colored('[-] ', 'yellow') + colored(Title, 'red'), end=End)
     elif Error_Level == Error_Levels.Alert:
-        print(colored('[+] ', 'green') + colored(Title, 'white'))
+        print(colored('[+] ', 'green') + colored(Title, 'white'), end=End)
     elif Error_Level == Error_Levels.Info:
-        print(colored('[*] ', 'magenta') + colored(Title, 'white'))
+        print(colored('[*] ', 'magenta') + colored(Title, 'white'), end=End)
     else:
-        print(Title)
+        print(Title, end=End)
 
     # show description
     if Description != None:
